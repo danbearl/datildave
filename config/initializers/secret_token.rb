54a3a9905b2c20c05ce9731cd5cf19b8a25ae603
Hashrocket Workstation <dev@hashrocket.com>
2012-11-05 13:53:30 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Datildave::Application.config.secret_token = 'b4fb37a6e90892e56eeab27cc8750a59a710c1fc5e92da6269ea1deea4c5e994bcb36faf865b19c6ca2b3a942d3f98ae11d895929349a2ad784c2b87da795186'
