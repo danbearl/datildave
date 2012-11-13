1bee69ee6447338de0a4fdd6e197c8e24714d4cc
Hashrocket Workstation <dev@hashrocket.com>
2012-11-13 13:55:54 -0500

    Generate buildybuild:cms

--- config/routes.rb
+++ config/routes.rb
@@ -23,4 +23,12 @@ Datildave::Application.routes.draw do
 
   root :to => 'home#index'
 
+
+  resources :pages, only: [:new, :create]
+
+  #keep these at the bottom of your file. They should be the last routes.
+  get "/:slug", to: "pages#show", as: :slug
+  get "/:slug/edit", to: "pages#edit", as: :edit_slug
+  put "/:slug", to: "pages#update", as: :slug
+  post "/:slug", to: "pages#destroy", as: :slug
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,14 @@
 
 ActiveRecord::Schema.define(:version => 20121113182802) do
 
+  create_table "pages", :force => true do |t|
+    t.string   "name"
+    t.string   "slug"
+    t.text     "body"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
   create_table "products", :force => true do |t|
     t.string   "name"
     t.integer  "quantity"
