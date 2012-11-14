223181604629e9581e642e0d0afb8f5ae527e36a
Hashrocket Workstation <dev@hashrocket.com>
2012-11-14 16:33:00 -0500

    Build contact model, controller and view.

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,4 +1,19 @@
 class ContactController < ApplicationController
   def new
+    @message = Message.new
   end
+
+  def create
+    @message = Message.new(params[:message])
+
+    if @message.valid?
+      Contact.new_message(@message).deliver
+
+      redirect_to root_path, notice: "Message successfully sent."
+    else
+      flash.now.alert = "Please fill in all fields correctly."
+      render "new"
+    end
+  end
+
 end
--- app/mailers/contact.rb
+++ app/mailers/contact.rb
@@ -1,3 +1,11 @@
 class Contact < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "contact@datildave.com"
+  default to: "dan.bearl@gmail.com"
+
+  def new_message(message)
+    @message = message
+
+    mail(:subject => "[Datil Dave Contact]: #{message.subject}")
+  end
+  
 end
