283855c2e332f19bbefc868a3ac47111c9678686
Dan Bearl <dan.bearl@gmail.com>
2014-04-14 21:41:03 -0400

    Implement strong parameters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   expose(:user)
 
   def create
-    @user = User.new(params[:user])
+    @user = User.new(user_params)
 
     if @user.save
       redirect_to root_url, notice: "User successfully created."
