2ad0c36d966d0bbf8a0e76519be0616aa44e096e
Dan Bearl <dan.bearl@gmail.com>
2015-08-24 23:54:34 -0400

    Cart#show redirects to :root when cart is empty

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,6 +1,12 @@
 class CartController < ApplicationController
   expose(:pages)
 
+  def show
+    if session[:cart].is_a? NilClass
+      redirect_to :root
+    end
+  end
+
   def empty_cart
     session[:cart] = []
     redirect_to "/cart/", notice: "Cart emptied!"
